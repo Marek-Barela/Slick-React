7a6f3c630774f0341b5b7738222ded60a9c34244
Marek Bareła <marek.barela@wp.pl>
2019-05-19 17:38:46 +0200

    basic hero styling commited

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import layerImg from '../../images/intro.png';
 import styles from './index.module.css';
 
 const Hero = () => {
-  const { hero, overlay, objectOne, objectTwo, wrapper } = styles;
+  const {
+    hero,
+    overlay,
+    objectOne,
+    objectTwo,
+    wrapper,
+    textContainer,
+    imageContainer
+  } = styles;
   return (
     <section className={hero}>
       <div className={overlay}>
@@ -10,7 +19,25 @@ const Hero = () => {
         <span className={objectTwo}></span>
       </div>
       <div className={wrapper}>
-
+        <div className={textContainer}>
+          <h1>
+            Handcrafted Web Template
+            For Business and Startups
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab
+            dolores ea fugiat nesciunt quisquam.
+          </p>
+          <button>
+            text
+          </button>
+          <button>
+            text
+          </button>
+        </div>
+        <div className={imageContainer}>
+          <img src={layerImg} />
+        </div>
       </div>
     </section>
   )
