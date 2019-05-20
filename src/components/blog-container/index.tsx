7730be688125002602c9f009ea0301aea848dd43
Marek Bareła <marek.barela@wp.pl>
2019-05-20 23:28:41 +0200

    blog basic structure commited

---
+++ src/components/blog-container/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./index.module.css";
+
+const BlogContainer = () => {
+  const { wrapper } = styles;
+  return <div className={wrapper} />;
+};
+
+export default BlogContainer;
