322b4b23abaca1b1a11ca40f060662c6a220a1d3
Marek Bareła <marek.barela@wp.pl>
2019-05-20 21:57:04 +0200

    team basic structure commited

---
+++ src/components/team-container/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./index.module.css";
+
+const Team = () => {
+  const { wrapper } = styles;
+  return <div className={wrapper} />;
+};
+
+export default Team;
