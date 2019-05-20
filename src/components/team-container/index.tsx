5ec91a8ebc725051e7a6c526e25d9af7edd47148
Marek Bareła <marek.barela@wp.pl>
2019-05-20 22:53:23 +0200

    team section commited

--- src/components/team-container/index.tsx
+++ src/components/team-container/index.tsx
@@ -1,9 +1,49 @@
 import React from "react";
+import TeamMember from "../team-member";
+import memberOne from "../../images/01.jpg";
+import memberTwo from "../../images/02.jpg";
+import memberThree from "../../images/03.jpg";
+import memberFour from "../../images/04.jpg";
 import styles from "./index.module.css";
 
-const Team = () => {
+const TeamContainer = () => {
   const { wrapper } = styles;
-  return <div className={wrapper} />;
+  return (
+    <div className={wrapper}>
+      <TeamMember
+        image={memberOne}
+        name="Patric Green"
+        title="Lead Designer"
+        facebook="https://www.facebook.com"
+        twitter="https://twitter.com"
+        google="https://plus.google.com/"
+      />
+      <TeamMember
+        image={memberTwo}
+        name="Celina D Cruze"
+        title="Front-end Developer"
+        facebook="https://www.facebook.com"
+        twitter="https://twitter.com"
+        google="https://plus.google.com/"
+      />
+      <TeamMember
+        image={memberThree}
+        name="Daryl Dixon"
+        title="Content Writer"
+        facebook="https://www.facebook.com"
+        twitter="https://twitter.com"
+        google="https://plus.google.com/"
+      />
+      <TeamMember
+        image={memberFour}
+        name="Susan Parker"
+        title="Support Engineer"
+        facebook="https://www.facebook.com"
+        twitter="https://twitter.com"
+        google="https://plus.google.com/"
+      />
+    </div>
+  );
 };
 
-export default Team;
+export default TeamContainer;
