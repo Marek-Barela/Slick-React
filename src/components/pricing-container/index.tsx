986af7932063b5e405c47b309bfef25a45e4cbd5
Marek Bareła <marek.barela@wp.pl>
2019-05-20 21:06:11 +0200

    pricing content update

--- src/components/pricing-container/index.tsx
+++ src/components/pricing-container/index.tsx
@@ -6,9 +6,33 @@ const PricingContainer = () => {
   const { wrapper } = styles;
   return (
     <div className={wrapper}>
-      <PricingItem title="FREE" cost="0" />
-      <PricingItem title="STANDARD" cost="19.99" />
-      <PricingItem title="BUSINESS" cost="29.99" />
+      <PricingItem
+        title="FREE"
+        cost="0"
+        storage="500MB Storage"
+        users="Single User"
+        GB="5 GB Bandwith"
+        features="Minimal Features"
+        dashboard="No Dashboard"
+      />
+      <PricingItem
+        title="STANDARD"
+        cost="19.99"
+        storage="2 GB Storage"
+        users="Upto 2 users"
+        GB="50 GB Bandwith"
+        features="All Features"
+        dashboard="Sales Dashboard"
+      />
+      <PricingItem
+        title="BUSINESS"
+        cost="29.99"
+        storage="5 GB Storage"
+        users="Upto 4 users"
+        GB="100 GB Bandwith"
+        features="All Features"
+        dashboard="Sales Dashboard"
+      />
     </div>
   );
 };
--- src/components/pricing-item/index.tsx
+++ src/components/pricing-item/index.tsx
@@ -6,11 +6,16 @@ import styles from "./index.module.css";
 interface Props {
   title: string;
   cost: string;
+  storage: string;
+  users: string;
+  GB: string;
+  features: string;
+  dashboard: string;
 }
 
 const PricingItem: FC<Props> = props => {
   const { wrapper, itemWrapper, price, list } = styles;
-  const { title, cost } = props;
+  const { title, cost, storage, users, GB, features, dashboard } = props;
   return (
     <div className={wrapper}>
       <div className={itemWrapper}>
@@ -18,11 +23,11 @@ const PricingItem: FC<Props> = props => {
         <span className={price}>{cost}</span>
         <ul className={list}>
           <ListItem text="Free Instalation" />
-          <ListItem text="500MB Storage" />
-          <ListItem text="Single User" />
-          <ListItem text="5 GB Bandwith" />
-          <ListItem text="Minimal Features" />
-          <ListItem text="No Dashboard" />
+          <ListItem text={storage} />
+          <ListItem text={users} />
+          <ListItem text={GB} />
+          <ListItem text={features} />
+          <ListItem text={dashboard} />
         </ul>
         <Button
           url="https://uideck.com/products/slick-free-bootstrap-template/"
