31681e7faa52a2f72adbfa8777423a87a1c54792
Marek Bareła <marek.barela@wp.pl>
2019-08-31 11:09:31 +0200

    footer commited

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AuthorContainer from "../footer-author-container";
+
+const Footer = () => {
+  return (
+    <footer>
+      <AuthorContainer />
+    </footer>
+  );
+};
+
+export default Footer;
